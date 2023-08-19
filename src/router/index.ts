4c876c7e1809e71130c4145579bd40281129ac65
Hamel Barrero <hamel_-barrero@hotmail.com>
2023-08-19 16:32:54 -0500

    add view auth login

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      component: () => import('../views/IndexView.vue'),
+    },
+    {
       path: '/auth',
       name: 'auth',
       children: [
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,7 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h2>Login</h2>
+  <div class="center">
+    <div class="card">
+      <h2>Login</h2>
+      <form class="form">
+        <div class="form__group">
+          <input type="text" class="form__input" />
+        </div>
+        <div class="form__group">
+          <input type="password" class="form__input" />
+        </div>
+        <div class="form__button">
+          <button class="button button__purple">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
