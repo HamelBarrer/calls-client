88341c414c52af4ef87d8f078fe12f49b9f1f747
Hamel Barrero <hamel_-barrero@hotmail.com>
2023-08-18 19:52:21 -0500

    add protected router and new view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,26 @@
+import { useAuthStore } from '@/stores/authStore';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/auth',
+      name: 'auth',
+      children: [
+        {
+          path: '',
+          name: 'login',
+          component: () => import('../views/auth/LoginView.vue'),
+        },
+      ],
+    },
+    {
       path: '/app',
       name: 'app',
+      meta: {
+        required: true,
+      },
       children: [
         {
           path: '',
@@ -15,6 +30,19 @@ const router = createRouter({
       ],
     },
   ],
+});
+
+router.beforeEach((to, from) => {
+  const authStore = useAuthStore();
+  const isPublic = to.matched.some((record) => record.meta.required);
+
+  if (isPublic && authStore.authenticatedUser.userId === 0) {
+    return { name: 'login' };
+  }
+
+  if (authStore.authenticatedUser.userId !== 0 && to.name === 'auth') {
+    return { name: 'home' };
+  }
 });
 
 export default router;
