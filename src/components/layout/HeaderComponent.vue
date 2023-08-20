43e4225599fa33ce5948a50da549e6a72b0a1a45
Hamel Barrero <hamel_-barrero@hotmail.com>
2023-08-19 21:39:54 -0500

    add header

---
+++ src/components/layout/HeaderComponent.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/authStore';
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__content">
+      <div class="header__opt">
+        <img
+          :src="authStore.authenticatedUser.avatar"
+          class="img__user"
+          alt="Imagen de usuario"
+        />
+        <span>{{ authStore.authenticatedUser.username }}</span>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  padding: 0 1rem;
+  border-bottom: 1px solid var(--border);
+}
+
+.header__content {
+  display: flex;
+  justify-content: end;
+}
+
+.header__opt {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+</style>
--- src/views/app/HomeView.vue
+++ src/views/app/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import HeaderComponent from '@/components/layout/HeaderComponent.vue';
 import SidebarComponent from '@/components/layout/SidebarComponent.vue';
 </script>
 
 <template>
   <div class="wrapper">
+    <HeaderComponent />
     <SidebarComponent />
     <main class="main">
       <h2>Hello</h2>
